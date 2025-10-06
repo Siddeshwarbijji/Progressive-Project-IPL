208b00333f081c622bfecfda41fcae25a97d1abe
username <wecp-dummy@gmail.com>
2025-10-06 13:02:49 +0000

    Day13

--- backend/src/main/java/com/wecp/progressive/jwt/JwtRequestFilter.java
+++ backend/src/main/java/com/wecp/progressive/jwt/JwtRequestFilter.java
@@ -1,5 +1,65 @@
 package com.wecp.progressive.jwt;
 
-public class JwtRequestFilter  {
+import java.io.IOException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.wecp.progressive.service.impl.UserLoginServiceImpl;
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter{
+    
+    private final JwtUtil jwtUtil;
+    private final UserLoginServiceImpl userLoginServiceImpl;
+
+    @Autowired
+    public JwtRequestFilter(JwtUtil jwtUtil, UserLoginServiceImpl userLoginServiceImpl) {
+        this.jwtUtil = jwtUtil;
+        this.userLoginServiceImpl = userLoginServiceImpl;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            String token = authHeader.substring(7);
+
+            String username = jwtUtil.extractUsername(token);
+
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetailsObj = userLoginServiceImpl.loadUserByUsername(username);
+
+                if(jwtUtil.validateToken(token, userDetailsObj)){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetailsObj,null,userDetailsObj.getAuthorities()
+                    );
+
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+                
+                }
+                
+            }
+
+            
+        }
+
+        filterChain.doFilter(request, response);
+    }
+    
+    
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplArraylist.java
@@ -1,5 +1,50 @@
 package com.wecp.progressive.service.impl;
 
-public class TeamServiceImplArraylist  {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.entity.Team;
+import com.wecp.progressive.service.TeamService;
+
+@Service
+public class TeamServiceImplArraylist implements TeamService {
+
+    List<Team> teams = new ArrayList<>();
+    public TeamServiceImplArraylist(){
+    teams.add(new Team(1,"RCB","Bangalore","KF",2007));
+    teams.add(new Team(2,"SRH","Hyderabad","SUNNetwork",2013));
+    }
+    @Override
+    public int addTeam(Team team) {
+
+        teams.add(team);
+        return teams.size();
+    }
+
+    @Override
+    public void emptyArrayList() {
+
+        teams = new ArrayList<>();
+    }
+
+    @Override
+    public List<Team> getAllTeams() {
+
+        return teams;
+    }
+
+    @Override
+    public List<Team> getAllTeamsSortedByName() {
+
+        // List<Team> sortedTeams = teams;
+        // sortedTeams.sort(Comparator.comparing(Team :: getTeamName));
+        Collections.sort(this.teams);
+        return teams;
+    }
+
+    
 }
