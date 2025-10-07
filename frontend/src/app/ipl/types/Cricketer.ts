a215eaebdafe2c40ca9d0ab9f555949d0156f1c8
username <wecp-dummy@gmail.com>
2025-10-07 12:42:28 +0000

    Day16

--- frontend/src/app/ipl/types/Cricketer.ts
+++ frontend/src/app/ipl/types/Cricketer.ts
@@ -1,6 +1,31 @@
 export class Cricketer {
-  
+  cricketerId:number;
+  teamId:number;
+  cricketerName:string;
+  age:number;
+  nationality: string;
+  experience: number;
+  role: string;
+  totalRuns: number;
+  totalWicketes: number;
 
+  constructor(cricketerId:number,teamId:number,cricketerName:string, age:number,nationality: string, experience: number, role: string, totalRuns: number, totalWicketes: number){
+    this.cricketerId = cricketerId;
+    this.teamId = teamId;
+    this.cricketerName = cricketerName;
+    this.age = age;
+    this.nationality = nationality;
+    this.experience = experience;
+    this.role = role;
+    this.totalRuns = totalRuns;
+    this.totalWicketes = totalWicketes;
+  }
+
+  displayInfo():void{
+    console.log(`Cricketer ID: ${this.cricketerId}`);//, Team ID: ${this.teamId}, Name: ${this.cricketerName}`);
+    console.log(`Team ID: ${this.teamId}`);
+    console.log(`Cricketer Name: ${this.cricketerName}`);
+  }
    
 }
 
